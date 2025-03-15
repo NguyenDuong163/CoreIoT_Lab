c82f2c4afa87830c589635e94b66d89da5821c2c
LAPTOP-R91CRERV\84869 <duong.nguyenwt16@hcmut.edu.vn>
2025-03-15 21:06:48 +0700

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "ExternalLedControl.h"
+
 #define LED_PIN 48
 #define SDA_PIN GPIO_NUM_11
 #define SCL_PIN GPIO_NUM_12
@@ -10,13 +12,10 @@
 #include <ArduinoOTA.h>
 #include <Adafruit_NeoPixel.h>
 
-Adafruit_NeoPixel pixels(4, 8 , NEO_GRB + NEO_KHZ800); // D5-D6
-volatile bool neoPixelState = false; // Trạng thái của LED NeoPixel
+constexpr char WIFI_SSID[] = "DCrab";
+constexpr char WIFI_PASSWORD[] = "zzzzzzzz";
 
-constexpr char WIFI_SSID[] = "Duong";
-constexpr char WIFI_PASSWORD[] = "ssssssss";
-
-constexpr char TOKEN[] = "o7727m2w21u0gnfj0urc";
+constexpr char TOKEN[] = "6e2vlzqcf3ci6cd2p8n3";
 
 constexpr char THINGSBOARD_SERVER[] = "app.coreiot.io";
 constexpr uint16_t THINGSBOARD_PORT = 1883U;
@@ -28,26 +27,27 @@ constexpr char MQTT_PASSWORD[] = "ug6bx7oenuthcceh9xzs";
 constexpr uint32_t MAX_MESSAGE_SIZE = 1024U;
 constexpr uint32_t SERIAL_DEBUG_BAUD = 115200U;
 
-constexpr char BLINKING_INTERVAL_ATTR[] = "blinkingInterval";
+// constexpr char BLINKING_INTERVAL_ATTR[] = "blinkingInterval";
 constexpr char LED_MODE_ATTR[] = "ledMode";
 constexpr char LED_STATE_ATTR[] = "ledState";
 
 volatile bool attributesChanged = false;
+
 volatile int ledMode = 0;
 volatile bool ledState = false;
 
-constexpr uint16_t BLINKING_INTERVAL_MS_MIN = 10U;
-constexpr uint16_t BLINKING_INTERVAL_MS_MAX = 60000U;
-volatile uint16_t blinkingInterval = 1000U;
+// constexpr uint16_t BLINKING_INTERVAL_MS_MIN = 10U;
+// constexpr uint16_t BLINKING_INTERVAL_MS_MAX = 60000U;
+// volatile uint16_t blinkingInterval = 1000U;
 
 uint32_t previousStateChange;
 
 constexpr int16_t telemetrySendInterval = 10000U;
 uint32_t previousDataSend;
 
-constexpr std::array<const char *, 2U> SHARED_ATTRIBUTES_LIST = {
+constexpr std::array<const char *, EPC_1> SHARED_ATTRIBUTES_LIST = {
   LED_STATE_ATTR,
-  BLINKING_INTERVAL_ATTR
+  // BLINKING_INTERVAL_ATTR
 };
 
 WiFiClient wifiClient;
@@ -55,7 +55,7 @@ Arduino_MQTT_Client mqttClient(wifiClient);
 ThingsBoard tb(mqttClient, MAX_MESSAGE_SIZE);
 
 DHT20 dht20;
-
+//############RPC############//
 RPC_Response setLedSwitchState(const RPC_Data &data) {
     Serial.println("Received Switch state");
     bool newState = data;
@@ -64,33 +64,31 @@ RPC_Response setLedSwitchState(const RPC_Data &data) {
 
     digitalWrite(LED_PIN, newState);
 
-    neoPixelState = newState;
+    if (newState) {
+      tb.sendTelemetryData("in_LED_Status", "ON");
+    } else {
+      tb.sendTelemetryData("in_LED_Status", "OFF");
+    }
+    // neoPixelState = newState;
 
     attributesChanged = true;
     return RPC_Response("setLedSwitchValue", newState);
 }
-
+//##########################//
 const std::array<RPC_Callback, 1U> callbacks = {
   RPC_Callback{ "setLedSwitchValue", setLedSwitchState }
 };
 
 void processSharedAttributes(const Shared_Attribute_Data &data) {
   for (auto it = data.begin(); it != data.end(); ++it) {
-    if (strcmp(it->key().c_str(), BLINKING_INTERVAL_ATTR) == 0) {
-      const uint16_t new_interval = it->value().as<uint16_t>();
-      if (new_interval >= BLINKING_INTERVAL_MS_MIN && new_interval <= BLINKING_INTERVAL_MS_MAX) {
-        blinkingInterval = new_interval;
-        Serial.print("Blinking interval is set to: ");
-        Serial.println(new_interval);
-      }
-    } else if (strcmp(it->key().c_str(), LED_STATE_ATTR) == 0) {
+     if (strcmp(it->key().c_str(), LED_STATE_ATTR) == 0) {
       ledState = it->value().as<bool>();
       digitalWrite(LED_PIN, ledState);
       Serial.print("LED state is set to: ");
       Serial.println(ledState);
     }
   }
-  attributesChanged = true;
+  // attributesChanged = true;
 }
 
 const Shared_Attribute_Callback attributes_callback(&processSharedAttributes, SHARED_ATTRIBUTES_LIST.cbegin(), SHARED_ATTRIBUTES_LIST.cend());
@@ -103,7 +101,7 @@ void InitWiFi() {
   while (WiFi.status() != WL_CONNECTED) {
     // Delay 500ms until a connection has been successfully established
     delay(500);
-    Serial.print(".");
+    Serial.print(">");
   }
   Serial.println("Connected to AP");
 }
@@ -120,25 +118,6 @@ const bool reconnect() {
 }
 
 //############RTOS task############//
-void neoPixelTask(void *pvParameters) {
-   pixels.begin();
-   pixels.clear(); // Đảm bảo LED tắt khi khởi động
-   pixels.show();
-
-  while (true) {
-    if (neoPixelState) {
-      pixels.setPixelColor(0, pixels.Color(255, 255, 0)); // Xanh lá khi bật
-    } else {
-      pixels.setPixelColor(0, pixels.Color(0, 0, 0)); // Đỏ khi tắt
-    }
-    pixels.show();
-    
-    vTaskDelay(100 / portTICK_PERIOD_MS); // Kiểm tra mỗi 100ms
-  }
-}
-
-
-
 void connectToWiFi(void * parameter) {
   while (true) {
     if (!reconnect()) {
@@ -152,13 +131,27 @@ void connectToWiFi(void * parameter) {
 void coreIoTConnectTask(void *pvParameters) {
   while (true) {
     if (!tb.connected()) {
+      // Serial.print("Connecting to: ");
+      // Serial.print(THINGSBOARD_SERVER);
+      // Serial.print(" with token ");
+      // Serial.println(TOKEN);
+      //Use TOKEN (for CP)
+      //Use MQTT (for IoT)
+      // if (!tb.connect(THINGSBOARD_SERVER, TOKEN, THINGSBOARD_PORT)) {
+      //   Serial.println("Failed to connect");
+      //   // vTaskDelay(10 / portTICK_PERIOD_MS);
+      //   continue;
+      // }
+
+      //    USE MQTT
       Serial.print("Connecting to: ");
       Serial.print(THINGSBOARD_SERVER);
-      Serial.print(" with token ");
-      Serial.println(TOKEN);
+      Serial.print(" with MQTT_USER: ");
+      Serial.print(MQTT_USER);
+      Serial.print(" and MQTT_PASSWORD: ");
+      Serial.println(MQTT_PASSWORD);
       if (!tb.connect(THINGSBOARD_SERVER, MQTT_USER, THINGSBOARD_PORT, MQTT_CLIENT_ID, MQTT_PASSWORD)) {
         Serial.println("Failed to connect");
-        // vTaskDelay(10 / portTICK_PERIOD_MS);
         continue;
       }
 
@@ -167,6 +160,13 @@ void coreIoTConnectTask(void *pvParameters) {
       Serial.println("Subscribing for RPC...");
       if (!tb.RPC_Subscribe(callbacks.cbegin(), callbacks.cend())) {
         Serial.println("Failed to subscribe for RPC");
+        // vTaskDelay(10 / portTICK_PERIOD_MS);
+        continue;
+      }
+
+      // Đăng ký callback cho setExternLed //@
+      if (!tb.RPC_Subscribe(exLed_callbacks.cbegin(), exLed_callbacks.cend())) {
+        Serial.println("Failed to subscribe for external LED RPC");
         // vTaskDelay(10 / portTICK_PERIOD_MS);
         continue;
       }
@@ -248,92 +248,17 @@ void setup() {
 
   Wire.begin(SDA_PIN, SCL_PIN);
   dht20.begin();
+  // initLed();
   
+  // xTaskCreate(gradientEffectTask, "gradientEffectTask", 2048, NULL, 2, NULL);
   xTaskCreate(connectToWiFi, "connectToWiFi", 4096, NULL, 1, NULL);
   xTaskCreate(coreIoTConnectTask, "coreIoTConnectTask", 4096, NULL, 1, NULL);
   xTaskCreate(sendAtributesTask, "sendAtributesTask", 4096, NULL, 2, NULL);
   xTaskCreate(sendTelemetryTask, "sendTelemetryTask", 4096, NULL, 2, NULL);
-  xTaskCreate(tbLoopTask, "tbLoopTask", 2048, NULL, 1, NULL);
+  xTaskCreate(tbLoopTask, "tbLoopTask", 4096, NULL, 1, NULL);
   xTaskCreate(neoPixelTask, "neoPixelTask", 2048, NULL, 2, NULL);
 }
 
 void loop() {
-  // delay(10);
-
-  // if (!reconnect()) {
-  //   return;
-  // }
-
-  // if (!tb.connected()) {
-  //   Serial.print("Connecting to: ");
-  //   Serial.print(THINGSBOARD_SERVER);
-  //   Serial.print(" with token ");
-  //   Serial.println(TOKEN);
-  //   if (!tb.connect(THINGSBOARD_SERVER, TOKEN, THINGSBOARD_PORT)) {
-  //     Serial.println("Failed to connect");
-  //     return;
-  //   }
-
-  //   tb.sendAttributeData("macAddress", WiFi.macAddress().c_str());
-
-  //   Serial.println("Subscribing for RPC...");
-  //   if (!tb.RPC_Subscribe(callbacks.cbegin(), callbacks.cend())) {
-  //     Serial.println("Failed to subscribe for RPC");
-  //     return;
-  //   }
-
-  //   if (!tb.Shared_Attributes_Subscribe(attributes_callback)) {
-  //     Serial.println("Failed to subscribe for shared attribute updates");
-  //     return;
-  //   }
-
-  //   Serial.println("Subscribe done");
-
-  //   if (!tb.Shared_Attributes_Request(attribute_shared_request_callback)) {
-  //     Serial.println("Failed to request for shared attributes");
-  //     return;
-  //   }
-  // }
-
-  // if (attributesChanged) {
-  //   attributesChanged = false;
-  //   tb.sendAttributeData(LED_STATE_ATTR, digitalRead(LED_PIN));
-  // }
-
-  // if (ledMode == 1 && millis() - previousStateChange > blinkingInterval) {
-  //   previousStateChange = millis();
-  //   digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-  //   Serial.print("LED state changed to: ");
-  //   Serial.println(!digitalRead(LED_PIN));
-  // }
-
-  // if (millis() - previousDataSend > telemetrySendInterval) {
-  //   previousDataSend = millis();
-
-  //   dht20.read();
-    
-  //   float temperature = dht20.getTemperature();
-  //   float humidity = dht20.getHumidity();
-
-  //   if (isnan(temperature) || isnan(humidity)) {
-  //     Serial.println("Failed to read from DHT20 sensor!");
-  //   } else {
-  //     Serial.print("Temperature: ");
-  //     Serial.print(temperature);
-  //     Serial.print(" °C, Humidity: ");
-  //     Serial.print(humidity);
-  //     Serial.println(" %");
-
-  //     tb.sendTelemetryData("temperature", temperature);
-  //     tb.sendTelemetryData("humidity", humidity);
-  //   }
-
-  //   tb.sendAttributeData("rssi", WiFi.RSSI());
-  //   tb.sendAttributeData("channel", WiFi.channel());
-  //   tb.sendAttributeData("bssid", WiFi.BSSIDstr().c_str());
-  //   tb.sendAttributeData("localIp", WiFi.localIP().toString().c_str());
-  //   tb.sendAttributeData("ssid", WiFi.SSID().c_str());
-  // }
-
-  // tb.loop();
+  
 }
